d9e5d340a1e3dfc8864afaadce42f30329992545
jwalker-swe <jwalker.swe@gmail.com>
2026-06-16 14:42:05 -0500

    Update: additional hover animations for technologies section

--- components/ListedTechnology.tsx
+++ components/ListedTechnology.tsx
@@ -7,7 +7,7 @@ type ListedTechnologyType = {
 
 export default function ListedTechnology({iconPath, techName}: ListedTechnologyType) {
 	return (
-		<div className={`flex justify-start gap-2 items-center text-primary hover:text-blue-500`}>
+		<div className={`flex justify-start gap-2 items-center text-primary hover:text-blue-500 hover:scale-105 transition-all ease-in-out duration-300`}>
 			<Image src={iconPath} width={28} height={28} alt="TypeScript Icon"/>
 			<span className={`font-semibold`}>{techName}</span>
 		</div>
